26ab435897312376c3b088047546fff73b0f8854
sandyml <sandramayla@gmail.com>
2023-03-29 13:53:10 -0400

    commit tree 563bf9d3f1c18c0121de34e12e282b9f19d52cc7

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,17 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends, action.payload],
+      };
+      case "friends/remove":
+        return {
+          friends: state.friends.filter((f) => f.id !== action.payload),
+        };
+        default:
+          return state;
+  }
 }
+
+// When our reducer receives "friends/add", it should return a new state with this friend object added to the friends array.
+// When removing a friend, instead of an object, the action will include an id key with an integer. Find the friend with the matching id and remove them. Thought of in another way, the reducer is really returning a new state with an array of friends that includes everyone except the removed friend.
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,15 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return { 
+        numberOfPresents: state.numberOfPresents + 1
+       };
+       default: 
+       return state;
+  }
 }
+
+// If the reducer receives a type set to "presents/increase", 
+// return a new state where the value of numberOfPresents is increased by one.
